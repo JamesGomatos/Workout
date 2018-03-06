cfb94998770e7fde5ae801e2583460f18d16dcb0
James Gomatos <goarmygomatos@gmail.com>
2018-03-06 01:54:15 -0500

    added database

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var express = require("express");
 var http = require("http");
 var path = require("path");
+var pg = require('pg');
 const PORT = process.env.PORT || 5000
 
 
 
 // Calls the express function to start a new express application
 var app = express()
+
+//This ensures that when you access your app using the /db route,
+// it will return all rows in the test_table table.
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+
 
 app.get("/random/:min/:max", function(req, res) {
   var min = parseInt(req.params.min);
